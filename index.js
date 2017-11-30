9893ef243cc858a2a677adfa1cb79840e699c479
Jarrett Cruger <jcrugzz@gmail.com>
2017-11-30 17:56:10 -0500

    make it so

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function tinythen(obj, method, ...args) {
+  return {
+    then: (resolve, reject) => {
+      function done(err, thing) {
+        if (err) return reject(err);
+        resolve(thing);
+      }
+      if (!args.length) return obj[method](done);
+      obj[method](...arg, done);
+    }
+  }
+}
